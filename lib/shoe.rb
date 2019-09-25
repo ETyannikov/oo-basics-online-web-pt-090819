4ebf6c75967b6c1c6897545b313a2e4aa78589e2
Eddy Tyannikov <etyannikov@gmail.com>
2019-09-25 21:02:02 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,16 +1,5 @@
 class Shoe
   attr_accessor :brand, :color, :size, :material, :condition
  
-  def initialize(title)
-    @title = title
-  end
- 
-  def title
-    @title
-  end
- 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
+
 end
